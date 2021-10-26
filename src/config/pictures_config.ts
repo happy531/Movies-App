f609e79fd4609aa745f4923fdb27b4bc605639fe
happy531 <jedrzejwesolowski177@gmail.com>
2021-10-26 17:45:41 +0200

    add pictures config

---
+++ src/config/pictures_config.ts
@@ -0,0 +1,15 @@
+// image with 300px and 500 px width
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const img_500 = "https://image.tmdb.org/t/p/w500";
+
+// unavailable image
+export const unavailable =
+  "https://www.movienewz.com/img/films/poster-holder.jpg";
+
+// unavailable landscape
+export const unavailableLandscape =
+  "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg";
+
+// no actor image
+export const noPicture =
+  "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
