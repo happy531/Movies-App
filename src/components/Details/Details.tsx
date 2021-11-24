2ac8d754a246551ba476380eb957bbcbc29c9a67
happy531 <jedrzejwesolowski177@gmail.com>
2021-11-24 14:49:05 +0100

    finish details page for desktop

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import VoteScore from "../UI/VoteScore";
 import YTtrailer from "../YTtrailer/YTtrailer";
-import { img_500, unavailable } from "../../config/pictures_config";
+import { img_300, unavailable } from "../../config/pictures_config";
 
 import classes from "./Details.module.scss";
 
@@ -23,36 +23,58 @@ interface Props {
 
 const Details: React.FC<Props> = ({ details, video, genres }) => {
   return (
-    <section className={classes.main_content}>
-      <div className={classes.poster}>
+    <section className={classes.details}>
+      <div className={classes.details_poster}>
         <img
           src={
             details.poster_path
-              ? `${img_500}/${details.poster_path}`
+              ? `${img_300}/${details.poster_path}`
               : unavailable
           }
           alt={details.title || details.name}
         />
       </div>
-      <section className={classes.details}>
+      <section className={classes.details_main}>
         <h1>{details.title || details.name}</h1>
-        <div className={classes.info}>
+        <div className={classes.details_main__info}>
           <p>{details.release_date || details.first_air_date}</p>
           <p>{genres}</p>
           <p>{details.runtime || details.episode_run_time || "N/A"} mins</p>
         </div>
-        <div className={classes.info}>
-          <div className={classes.vote_container}>
+        <div className={classes.details_main__info}>
+          <div className={classes.vote}>
             <VoteScore vote={details.vote_average} />
           </div>
           <YTtrailer trailer_url={`https://www.youtube.com/watch?v=${video}`} />
         </div>
-        <div className={classes.info}>
+        <div className={classes.details_main__info}>
           <span className={classes.tagline}>{details.tagline}</span>
         </div>
-        <div className={classes.overview}>
+        <div className={classes.details_main__overview}>
           <h1>Overview</h1>
-          <span style={{ marginTop: "5px" }}>{details.overview}</span>
+          <span>{details.overview}</span>
+        </div>
+        <div className={classes.details_main__additional}>
+          <span className={classes.slice}>
+            <span className={classes.slice__title}>Status</span>
+            <p className={classes.slice__text}>{details.status}</p>
+          </span>
+          <span className={classes.slice}>
+            <span className={classes.slice__title}>Budget</span>
+            <p className={classes.slice__text}>
+              {details.budget ? `$${details.budget}` : "N/A"}
+            </p>
+          </span>
+          <span className={classes.slice}>
+            <span className={classes.slice__title}>Revenue</span>
+            <p className={classes.slice__text}>
+              {details.revenue ? `$${details.revenue}` : "N/A"}
+            </p>
+          </span>
+          <span className={classes.slice}>
+            <span className={classes.slice__title}>Original Language</span>
+            <p className={classes.slice__text}>{details.original_language}</p>
+          </span>
         </div>
       </section>
     </section>
